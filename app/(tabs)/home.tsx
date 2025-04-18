d568277786634778aaf1b16c98fe9f3d3a850c9d
Gid3onJura <homebasekorlash@googlemail.com>
2025-04-18 19:18:35 +0200

    home screen, fetch dates

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,10 +1,66 @@
-import { View, Text } from "react-native"
-import React from "react"
+import { View, Text, FlatList } from "react-native"
+import React, { useEffect, useState } from "react"
+import { AuthStore, useAuthStore } from "@/store/authStore"
+import styles from "@/assets/styles/home.styles"
+import { DateItemProps } from "@/interfaces"
 
 export default function Home() {
+  const { token } = useAuthStore() as AuthStore
+  const [dates, setDates] = useState([])
+  const [refreshing, setRefreshing] = useState(false)
+
+  const fetchDates = async (refresh = false) => {
+    if (refresh) {
+      setRefreshing(true)
+    }
+
+    try {
+      const response = await fetch(process.env.EXPO_PUBLIC_API_BASE_URL + "/event", {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          ...(process.env.EXPO_PUBLIC_API_KEY && { "api-key": process.env.EXPO_PUBLIC_API_KEY }),
+          ...(token && { Authorization: `Bearer ${token}` }),
+        },
+      })
+
+      const data = await response.json()
+      if (data) {
+        setDates(data)
+        setRefreshing(false)
+      }
+    } catch (error) {
+      console.error("Error fetching dates:", error)
+      setRefreshing(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchDates()
+  }, [])
+
+  const renderDateItem = ({ item }: { item: DateItemProps }) => {
+    return (
+      <View style={styles.bookCard}>
+        <View style={styles.bookHeader}>
+          <View style={styles.userInfo}>
+            <Text style={styles.username}>{item.description}</Text>
+          </View>
+        </View>
+      </View>
+    )
+  }
+
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <FlatList
+        data={dates}
+        renderItem={renderDateItem}
+        keyExtractor={(item) => item.id.toString()}
+        contentContainerStyle={styles.listContainer}
+        showsVerticalScrollIndicator={false}
+      />
     </View>
   )
 }
---
+++ interfaces/index.ts
@@ -0,0 +1,12 @@
+export interface DateItemProps {
+  description: string
+  eventcolor: string | null
+  eventdate: string
+  eventdatetimefrom: string
+  eventdatetimeto: string
+  eventtype: string
+  id: number
+  override: boolean
+  repeating: boolean
+  repetitiontype: string
+}
